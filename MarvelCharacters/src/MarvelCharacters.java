944fe36f1cdea253134d1f79e5d8ba43ba3f48c7
minazp <minazp@users.noreply.github.com>
2018-08-12 17:01:41 +0100

    Add files via upload

---
+++ MarvelCharacters/src/MarvelCharacters.java
@@ -0,0 +1,11 @@
+
+public class MarvelCharacters {
+	public static void main(String[] args) {
+		try {
+			GetMarvelCharacters gmc = new GetMarvelCharacters();
+			gmc.getCharacters();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
